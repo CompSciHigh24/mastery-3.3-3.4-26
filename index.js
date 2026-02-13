738e4cc823341a09ae19aae03b196630a8fc37ac
CompSciHigh24 <csh-github@compscihigh.org>
2026-02-13 05:41:13 -0500

    Initialize Express server with middleware and routes
    Set up an Express server with placeholder routes and middleware comments.

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+// Make sure to install express via npm
+
+/**
+ * Add Middleware to log incoming requests
+ * It runs for every request before reaching the route handlers
+ */
+
+/**
+ * Add a route to /name that displays your name as plain text
+ */
+
+
+/**
+ * Add a route to /about that displays a JSON object with details describing yourself
+ */
+
+
+/**
+ * Add 404 Middleware 
+ */
+
+
+app.listen(3000, () => {
+    console.log(`Server is running.`);
+});
+
+/**
+ * Run your server and test your routes.
+ */
